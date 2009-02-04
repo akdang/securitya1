f691a1ad7780c55b20d2e74edcc89cf47ea6fdb2
akdang <akdang@c21d20c0-f240-11dd-9fdb-3debd5008f28>
2009-02-04 02:32:24 +0000

    Instruction parser.

--- trunk/test.java
+++ trunk/test.java
@@ -1,23 +1,36 @@
-import java.io.*
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 
 public class test {
 	
-	public String readFromFile(String fileName) {
-	    String DataLine = "";
-	    try {
-	      File inFile = new File(fileName);
-	      BufferedReader br = new BufferedReader(new InputStreamReader(
-	          new FileInputStream(inFile)));
-
-	      DataLine = br.readLine();
-	      br.close();
-	    } catch (FileNotFoundException ex) {
-	      return (null);
-	    } catch (IOException ex) {
-	      return (null);
-	    }
-	    return (DataLine);
-
-	  }
-
+	public static void main(String []args) {
+		ArrayList<String> write = new ArrayList<String>();
+		
+		int i = 0;
+		String s = "write      lobj           2323 3232        ";
+		StringTokenizer test = new StringTokenizer(s, " ", false);
+		while(i <= test.countTokens()) {
+			write.add(test.nextToken());
+			
+			i++;
+		}
+		
+		System.out.println(write);
+		
+		
+		
+		
+		
+		
+		
+//		String[] split = s.split("lobj");
+//		
+//		for(String token : split) {
+//			System.out.println("here: " + token);
+//			System.out.println(!token.isEmpty() && !token.contains("read"));
+//		}
+	}
 }
+		
+
