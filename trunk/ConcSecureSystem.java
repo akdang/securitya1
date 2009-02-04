6c2dcf7813e4a0c91ab310b8e9317360b94554ab
ccunni <ccunni@c21d20c0-f240-11dd-9fdb-3debd5008f28>
2009-02-04 02:15:58 +0000

    Just before adding Instruction parsing.

--- trunk/ConcSecureSystem.java
+++ trunk/ConcSecureSystem.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.*;
 
 class ConcSecureSystem {
 
@@ -22,41 +22,65 @@ class ConcSecureSystem {
 	rm.createNewObject("lobj", 0);
 	rm.addObjectLevel("lobj", SecurityLevel.LOW);
 
-	lyle.start();
+    lyle.start();
 	hal.start();
-    rm.tester();    
+
+    rm.printer();
     } // main
 }
 
 class ReferenceMonitor 
 {
     private ObjectManager om;
+    private HashMap<String, SecurityLevel> objectLevels;
+    private HashMap<String, SecurityLevel> subjectLevels;
+
+    //delete me
+    public void printer()
+    {
+        Iterator<String> it = objectLevels.keySet().iterator();
+        while(it.hasNext())
+        {
+            String tmp = it.next();
+            System.out.println("obj: " + tmp + "\tlvl: " + objectLevels.get(tmp));
+        }
+
+        it = subjectLevels.keySet().iterator();
+        while(it.hasNext())
+        {
+            String tmp = it.next();
+            System.out.println("subj: " + tmp + "\tlvl: " + subjectLevels.get(tmp));
+        }
+    }
 
     public ReferenceMonitor()
     {
         om = new ObjectManager();
-    }
-    public void tester()
-    {
-        om.createNewObj("Bob", 0);
-        System.out.println("Bob val (expecting 0):" + om.readObjVal("Bob"));
-        om.setObjVal("Bob", 1);
-        System.out.println("Bob val (expecting 1):" + om.readObjVal("Bob"));
-        om.readObjVal("Chris");
-    }
-    public void addSubjectLevel(String subjName, SecurityLevel s)
-    {
+        objectLevels = new HashMap<String, SecurityLevel>();
+        subjectLevels = new HashMap<String, SecurityLevel>();
     }
 
-    public void createNewObject(String objName, int objVal)
+    public synchronized void executeInstruction(String name, Instruction i)
     {
+        System.out.println("Executing instruction " + i + " for " + name + ".");
     }
     
-
-    //Todo: make this part of createObj?
-    public void addObjectLevel(String objName, SecurityLevel s)
+    public void addSubjectLevel(String name, SecurityLevel s)
     {
+        subjectLevels.put(name, s);
     }
+    
+    public void addObjectLevel(String name, SecurityLevel s)
+    {
+        objectLevels.put(name, s);
+    }
+
+    public void createNewObject(String name, int val)
+    {
+        om.createNewObject(name, val);
+    }
+    
+    
 
     private class ObjectManager
     {
@@ -67,12 +91,12 @@ class ReferenceMonitor
             objects = new HashMap<String,SecureObject>();
         }
 
-        private void createNewObj(String name, int val)
+        private void createNewObject(String name, int val)
         {
             objects.put(name, new SecureObject(name, val));
         }
 
-        private void setObjVal(String name, int val)
+        private void setObjectVal(String name, int val)
         {
             SecureObject tmp = objects.get(name);
             assert (tmp != null) : "No object mapped to key \"" + name + "\".";
@@ -80,7 +104,7 @@ class ReferenceMonitor
             tmp.setVal(val);
         }
 
-        private int readObjVal(String name)
+        private int readObjectVal(String name)
         {
             SecureObject tmp = objects.get(name);
             assert (tmp != null) : "No object mapped to key \"" + name + "\".";
@@ -118,14 +142,42 @@ class ReferenceMonitor
 
 }
 
-class SecureSubject
+class SecureSubject extends Thread
 {
+    private String name;
+    private SecurityLevel securityLevel;
+    private ReferenceMonitor referenceMonitor;
+    private ArrayList<Instruction> instructions;
+
     public SecureSubject(String name, SecurityLevel s, String fileName, ReferenceMonitor r)
     {
+        this.name = name;
+        this.securityLevel = s;
+        this.referenceMonitor = r;
+        this.instructions = new ArrayList<Instruction>();
     }
 
-    public void start()
+    public void run()
     {
+        System.out.println(name + " starting up.");
+
+        for(Instruction i: instructions)
+        {
+            referenceMonitor.executeInstruction(name, i);
+        }
+    }
+}
+
+class Instruction {
+    String s;
+    public String toString() { return s; }
+    public Instruction()
+    {
+        this.s = "blank";
+    }
+
+    public Instruction(String s)
+    { this.s = s;
     }
 }
 
