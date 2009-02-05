7afbcc6f166e24ed8657a689366433e443efcf05
ccunni <ccunni@c21d20c0-f240-11dd-9fdb-3debd5008f28>
2009-02-05 03:10:20 +0000

    Fixed thread sleeping and now setting all object names to lower case.

--- trunk/UnitTests.java
+++ trunk/UnitTests.java
@@ -2,6 +2,14 @@ import junit.framework.*;
 
 public class UnitTests extends TestCase 
 {
+	public void testSubjectConstruction()
+	{
+		ReferenceMonitor rm = new ReferenceMonitor();
+        SecureSubject hal = new SecureSubject("Hal", SecurityLevel.HIGH, "HalInstructions", rm);
+
+        //assertTrue(hal.getName().equals("));
+
+	}
 	public void testAdd() 
 	{
         ReferenceMonitor rm = new ReferenceMonitor();
